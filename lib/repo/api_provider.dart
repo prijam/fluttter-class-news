ea9ea0288b448c299d138588cd5d3f91de52c224
Prijam <prijam36@gmail.com>
2020-03-01 09:42:39 +0545

    1st_day

---
+++ lib/repo/api_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_news_app_bloc/models/item-model.dart';
+import 'package:http/http.dart';
+
+class ApiProvider {
+  static const String api = "https://hacker-news.firebaseio.com/v0";
+  Client client = Client();
+
+  fetchTopIds() async {
+    final response = await client.get("$api/topstories.jsony");
+    final id = jsonDecode(response.body);
+    debugPrint(id);
+    return id;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get("$api/item/$id.json");
+    final content = jsonDecode(response.body);
+    return ItemModel.fromJson(content);
+  }
+}
